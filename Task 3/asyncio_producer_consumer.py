0a08647c9ea4472536abbe3364660810f5e6c0e1
Георгий <dfffgfgf@yandex.ru>
2021-06-22 15:57:45 +0300

    add asyncio producer/consumer

---
+++ Task 3/asyncio_producer_consumer.py
@@ -0,0 +1,53 @@
+import asyncio
+import random
+
+warehouse = []
+max_elements = 10
+
+
+def is_overflow():
+    return len(warehouse) >= max_elements
+
+
+def is_underflow():
+    return len(warehouse) == 0
+
+
+def add_element(element):
+    warehouse.append(element)
+
+
+def delete_element():
+    return warehouse.pop()
+
+
+async def producer():
+    while True:
+        x = random.randint(a=1, b=10000)
+        if not is_overflow():
+            add_element(x)
+            print(f'Элемент {x} добавлен на склад')
+        else:
+            print('Склад заполнен')
+        await asyncio.sleep(random.randint(a=1, b=10))
+
+
+async def consumer():
+    while True:
+        if not is_underflow():
+            x = delete_element()
+            print(f'Элемент {x} взят со склада')
+        else:
+            print('Склад пуст')
+        await asyncio.sleep(random.randint(a=1, b=10))
+
+
+async def main():
+    task1 = asyncio.create_task(producer())
+    task2 = asyncio.create_task(consumer())
+
+    await asyncio.gather(task1, task2)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
