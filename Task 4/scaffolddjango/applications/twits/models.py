cd75b678189b820b747cac9c52fe6f593ff984c6
Георгий <dfffgfgf@yandex.ru>
2021-06-26 22:17:56 +0300

    fix flake8 issues

--- Task 4/scaffolddjango/applications/twits/models.py
+++ Task 4/scaffolddjango/applications/twits/models.py
@@ -2,37 +2,6 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 
-# class Message(models.Model):
-#     """
-#     Вопрос
-#     """
-#
-#     # Текст вопроса
-#     text = models.CharField(max_length=250, blank=True, null=False, default='')
-#
-#     class Meta:
-#         verbose_name = 'Message'
-#         verbose_name_plural = 'Messages'
-#
-#     def __str__(self):
-#         return self.text
-#
-#
-# class Customer(models.Model):
-#
-#     first_name = models.CharField(max_length=200, blank=False, null=False)
-#     last_name = models.CharField(max_length=200, blank=False, null=False)
-#
-#
-# class Good(models.Model):
-#     name = models.CharField(max_length=200, blank=False, null=False)
-#     price = models.FloatField(null=False, default=0.0)
-#
-#
-# class Order(models.Model):
-#     customer = models.ForeignKey(to=Customer, on_delete=models.CASCADE)
-#     goods = models.ManyToManyField(to=Good)
-
 User = get_user_model()
 
 
--- Task 4/scaffolddjango/applications/twits/serializers.py
+++ Task 4/scaffolddjango/applications/twits/serializers.py
@@ -1,32 +1,6 @@
 from rest_framework import serializers
 from applications.twits.models import Film, Comment, Review
 
-# class CustomerSerializer(serializers.ModelSerializer):
-#     full_name = serializers.SerializerMethodField()
-#
-#     @staticmethod
-#     def get_full_name(obj):
-#         return obj.first_name + " " + obj.last_name
-#
-#     class Meta:
-#         model = Customer
-#         exclude = ()
-#
-#
-# class OrderSerializer(serializers.ModelSerializer):
-#     customer = CustomerSerializer(many=False, read_only=False)
-#
-#     class Meta:
-#         model = Order
-#         exclude = ()
-#
-#
-# class MessageSerializer(serializers.ModelSerializer):
-#
-#     class Meta:
-#         model = Message
-#         exclude = ()
-
 
 class FilmSerializer(serializers.ModelSerializer):
     class Meta:
